a6539cfb168c576abe136182b7cd988ef8cb8216
weihsuankang <sammy00024@gmail.com>
2018-06-22 07:10:30 +0800

    7:10

--- leaderboard.c
+++ leaderboard.c
@@ -3,6 +3,7 @@
 typedef struct leaderboard {
 	char name1[20];
 	char name2[20];
+	char win[20];
 } Leaderboard;
 
 void rank(void) {
@@ -10,25 +11,26 @@ void rank(void) {
 	FILE *file;
 	char NAME1[20];
 	char NAME2[20];
+	char WIN[20];
 	int size = 0;
 
 	file = fopen("save.txt", "r");
 
 	while (!feof(file)) {
-		fscanf(file, "%s%s", NAME1, NAME2);
+		fscanf(file, "%s%s%s", NAME1, NAME2,WIN);
 		size++;
 	}
 	rewind(file);
 	Leaderboard player[size];
 	for (int i = 0; i < size; i++) {
-		fscanf(file, "%s%s", player->name1, player->name2);
+		fscanf(file, "%s%s%s", player->name1, player->name2,player->win);
 	}
 
 
 	printf("　 　      遊戲戰績\n");
 	printf("　 **************************\n");
 	for (int j = 0; j < size; j++) {
-		printf("　 第%d場      %-15s%2s\n", j + 1, player[j].name1, player[j].name2);
+		printf("　 第%d場      %-15s%2s%2s\n", j + 1, player[j].name1, player[j].name2,player[j].win);
 	}
 	printf("　 **************************\n");
 
